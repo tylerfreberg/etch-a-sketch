84f78a8f0250996b0e672805787214fbf36ec133
Tyler Freberg <tylerfreberg1@gmail.com>
2026-03-04 23:52:37 -0600

    added user input via a form input instead of a prompt. added shake animation if input was outside of allowed values (0 < gridSize < 101)

--- javascript.js
+++ javascript.js
@@ -1,40 +1,56 @@
-let container = document.querySelector("#container");
-let fragment = document.createDocumentFragment();
-let gridSize = 0
+let input = document.querySelector("form");
 
-while (gridSize < 1) {
-    gridSize = prompt("Please enter a desired grid size.");
-
-    if (gridSize < 1 || gridSize > 100) {
-        gridSize = 0;
+input.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let gridSize = input.querySelector("#gridSize").value;
+    if (gridSize > 0 && gridSize < 101) {
+        createGrid(gridSize);
+        console.log('success');
     }
-}
-container.style.setProperty("--gridSize", gridSize);
-
-for (i = 0; i < (gridSize*gridSize); i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "gray";
-    square.classList.add("square")
-    fragment.appendChild(square);
-}
-
-container.appendChild(fragment);
-
-let isDrawing = false;
-let squares = document.querySelectorAll(".square");
-
-container.addEventListener("mousedown", () => isDrawing = true)
-container.addEventListener("mouseup", () => isDrawing = false)
-
-squares.forEach(square => {
-
-    square.addEventListener("mouseover", () => {
-        if (isDrawing) {
-            square.style.backgroundColor = "white";
-        }
-    })
-
-    square.addEventListener("click", () => {
-        square.style.backgroundColor = "white";
-    })
+    else {
+        input.classList.add("shake-animation");
+        console.log('failure');
+        input.addEventListener("animationend", () => {
+            input.classList.remove("shake-animation");
+        })
+    }
 })
+
+
+function createGrid(val) {
+    console.log("grid generated");
+    let container = document.querySelector("#container");
+    let fragment = document.createDocumentFragment();
+    let gridSize = val;
+
+    container.innerText = "";
+    container.style.setProperty("--gridSize", gridSize);
+
+    for (i = 0; i < (gridSize*gridSize); i++) {
+        let square = document.createElement("div");
+        square.style.backgroundColor = "gray";
+        square.classList.add("square")
+        fragment.appendChild(square);
+    }
+
+    container.appendChild(fragment);
+
+    let isDrawing = false;
+    let squares = document.querySelectorAll(".square");
+
+    container.addEventListener("mousedown", () => isDrawing = true)
+    container.addEventListener("mouseup", () => isDrawing = false)
+
+    squares.forEach(square => {
+
+        square.addEventListener("mouseover", () => {
+            if (isDrawing) {
+                square.style.backgroundColor = "white";
+            }
+        })
+
+        square.addEventListener("click", () => {
+            square.style.backgroundColor = "white";
+        })
+    })
+}
